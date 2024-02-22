eef234d29a523a52e32912babe053b0166562f33
Ritu Raj <rituraj.onnet@gmail.com>
2024-02-22 14:42:20 +0530

    code changes

---
+++ web/modules/custom/custom_api/src/EventSubscriber/CustomRestResourceSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+// src/EventSubscriber/CustomRestResourceSubscriber.php
+
+namespace Drupal\custom_api\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CustomRestResourceSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Modifies REST resource responses.
+   */
+  public function onRespond(ViewEvent $event) {
+    $request = $event->getRequest();
+    $response = $event->getResponse();
+
+    // Check if the request corresponds to the desired REST resource.
+    if ($request->attributes->get('_route') == 'entity.node.canonical') {
+      // Modify the response content as needed.
+      $content = $response->getContent();
+      // Modify $content here.
+      $response->setContent($content);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::VIEW][] = ['onRespond'];
+    return $events;
+  }
+}
